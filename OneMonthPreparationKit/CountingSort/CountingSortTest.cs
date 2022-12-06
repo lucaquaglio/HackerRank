c2ac765c83db009c5b8b7de8332b2b5293e7e128
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2022-12-06 19:21:40 +0100

    Added CountingSortTest

---
+++ OneMonthPreparationKit/CountingSort/CountingSortTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.OneMonthPreparationKit.CountingSort.Testing
+{
+	sealed class CountingSortTest
+	{
+		[Test]
+		public void Sort()
+		{
+			var countingSort = new CountingSort();
+			var input = new[] { 1, 1, 3, 2, 1 }.ToList();
+			var result = countingSort.Sort(input);
+
+			var expectedResult = new[] { 1, 1, 1, 2, 3 }.ToList();
+			Assert.That(result, Is.EqualTo(expectedResult));
+		}
+	}
+}
