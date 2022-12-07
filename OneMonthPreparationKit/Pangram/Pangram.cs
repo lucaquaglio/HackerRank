f49b6a2db82b6e7d03218eacfefe22ec15426e8e
Luca Quaglio <luca.quaglio@hotmail.it>
2022-12-07 19:30:46 +0100

    Pangram (#3)
    Pangram

---
+++ OneMonthPreparationKit/Pangram/Pangram.cs
@@ -0,0 +1,23 @@
+﻿namespace HackerRank.OneMonthPreparationKit.Pangram
+{
+	sealed class Pangram
+	{
+		public static string IsPangram(string s)
+		{
+			var englishAlphabetList = englishAlphabet.ToList();
+			foreach (var letter in s.ToLower().ToArray())
+			{
+				englishAlphabetList.Remove(letter);
+			}
+
+			return englishAlphabetList.Count is 0
+				? PangramResult
+				: NotPangram;
+		}
+
+		static readonly char[] englishAlphabet = new [] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+		const string PangramResult = "pangram";
+		const string NotPangram = "not pangram";
+	}
+}
---
+++ OneMonthPreparationKit/Pangram/PangramTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.OneMonthPreparationKit.Pangram.Testing
+{
+	sealed class PangramTest
+	{
+		[Test]
+		public void IsPangram()
+		{
+			var result = Pangram.IsPangram("We promptly judged antique ivory buckles for the next prize");
+			Assert.That(result, Is.EqualTo("pangram"));
+
+			result = Pangram.IsPangram("All of the letters of the alphabet are present in the string.");
+			Assert.That(result, Is.EqualTo("not pangram"));
+		}
+	}
+}
