b591db258dce0f84f02cac8f493a6ce5905580b0
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2022-12-07 19:29:27 +0100

    Implement functionality

--- OneMonthPreparationKit/Pangram/Pangram.cs
+++ OneMonthPreparationKit/Pangram/Pangram.cs
@@ -4,7 +4,20 @@
 	{
 		public static string IsPangram(string s)
 		{
-			return null;
+			var englishAlphabetList = englishAlphabet.ToList();
+			foreach (var letter in s.ToLower().ToArray())
+			{
+				englishAlphabetList.Remove(letter);
+			}
+
+			return englishAlphabetList.Count is 0
+				? PangramResult
+				: NotPangram;
 		}
+
+		static readonly char[] englishAlphabet = new [] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+		const string PangramResult = "pangram";
+		const string NotPangram = "not pangram";
 	}
 }
