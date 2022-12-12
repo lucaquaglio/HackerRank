9dc6d1ed0cd333b8b6fae82ee26fb8baccf8efe3
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2022-12-12 21:43:43 +0100

    Rude but work

--- OneMonthPreparationKit/SubarrayDivision1/SubarrayDivision1.cs
+++ OneMonthPreparationKit/SubarrayDivision1/SubarrayDivision1.cs
@@ -4,7 +4,36 @@
 	{
 		public static int Birthday(List<int> s, int d, int m)
 		{
-			return -1;
+			var waysToDivideChocolate = new List<string>();
+			for (int i = 0; i <= s.Count - m; i++)
+			{
+				var square = s[i];
+				var segments = new Stack<int>();
+				segments.Push(square);
+
+				foreach (var squareToAdd in s.Skip(i + 1))
+				{
+					var squareSum = segments.Sum() + squareToAdd;
+					if (squareSum <= d)
+					{
+						segments.Push(squareToAdd);
+
+						if (segments.Count == m)
+						{
+							if (squareSum == d)
+							{
+								waysToDivideChocolate.Add(string.Join(':', segments));
+							}
+							else
+							{
+								segments.Pop();
+							}
+						}
+					}
+				}
+			}
+
+			return waysToDivideChocolate.Distinct().Count();
 		}
 	}
 }
--- OneMonthPreparationKit/SubarrayDivision1/SubarrayDivision1Test.cs
+++ OneMonthPreparationKit/SubarrayDivision1/SubarrayDivision1Test.cs
@@ -15,11 +15,23 @@ namespace HackerRank.OneMonthPreparationKit.SubarrayDivision1.Testing
 			Assert.That(result, Is.EqualTo(2));
 		}
 
+		[Test]
 		public void Birthday_Case2()
 		{
 			var chocolateBar = new[] { 2, 2, 1, 3, 2 }.ToList();
 			var day = 6;
 			var month = 3;
+
+			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
+			Assert.That(result, Is.EqualTo(3));
+		}
+
+		[Test]
+		public void Birthday_Case3()
+		{
+			var chocolateBar = new[] { 4 }.ToList();
+			var day = 4;
+			var month = 1;
 
 			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
 			Assert.That(result, Is.EqualTo(2));
