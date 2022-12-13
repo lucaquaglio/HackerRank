b918397fd58f8b6c662175968112f566067ac07a
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2022-12-13 19:37:44 +0100

    Solution B

--- OneMonthPreparationKit/SubarrayDivision1/SubarrayDivision1.cs
+++ OneMonthPreparationKit/SubarrayDivision1/SubarrayDivision1.cs
@@ -4,36 +4,51 @@
 	{
 		public static int Birthday(List<int> s, int d, int m)
 		{
-			var waysToDivideChocolate = new List<string>();
-			for (int i = 0; i <= s.Count - m; i++)
+			var waysBarCanBeDivided = new List<string>();
+			for (int i = 0; i < s.Count; i++)
 			{
+				var segments = new List<int>();
 				var square = s[i];
-				var segments = new Stack<int>();
-				segments.Push(square);
 
-				foreach (var squareToAdd in s.Skip(i + 1))
+				if (square > d)
 				{
-					var squareSum = segments.Sum() + squareToAdd;
-					if (squareSum <= d)
-					{
-						segments.Push(squareToAdd);
+					continue;
+				}
 
-						if (segments.Count == m)
-						{
-							if (squareSum == d)
-							{
-								waysToDivideChocolate.Add(string.Join(':', segments));
-							}
-							else
-							{
-								segments.Pop();
-							}
-						}
+				segments.Add(square);
+
+				for (int j = i + 1; j < s.Count && segments.Count < m; j++)
+				{
+					var nextSquare = s[j];
+					var squareSum = segments.Sum() + nextSquare;
+					if (squareSum > d)
+					{
+						continue;
+					}
+
+					if (segments.Count < (m - 1) || squareSum == d)
+					{
+						segments.Add(nextSquare);
+					}
+				}
+
+				if (segments.Sum() == d && segments.Count == m)
+				{
+					var differenceToMonth = m - segments.Count;
+					for (int j = 0; j < differenceToMonth; j++)
+					{
+						segments.Add(0);
+					}
+
+					var way = string.Join(":", segments.OrderBy(x => x).ToArray());
+					if (!waysBarCanBeDivided.Contains(way))
+					{
+						waysBarCanBeDivided.Add(way);
 					}
 				}
 			}
 
-			return waysToDivideChocolate.Distinct().Count();
+			return waysBarCanBeDivided.Distinct().Count();
 		}
 	}
 }
--- OneMonthPreparationKit/SubarrayDivision1/SubarrayDivision1Test.cs
+++ OneMonthPreparationKit/SubarrayDivision1/SubarrayDivision1Test.cs
@@ -23,7 +23,7 @@ namespace HackerRank.OneMonthPreparationKit.SubarrayDivision1.Testing
 			var month = 3;
 
 			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
-			Assert.That(result, Is.EqualTo(3));
+			Assert.That(result, Is.EqualTo(2));
 		}
 
 		[Test]
@@ -32,6 +32,28 @@ namespace HackerRank.OneMonthPreparationKit.SubarrayDivision1.Testing
 			var chocolateBar = new[] { 4 }.ToList();
 			var day = 4;
 			var month = 1;
+
+			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
+			Assert.That(result, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void Birthday_Case4()
+		{
+			var chocolateBar = new[] { 2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1, }.ToList();
+			var day = 18;
+			var month = 7;
+
+			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
+			Assert.That(result, Is.EqualTo(3));
+		}
+
+		[Test]
+		public void Birthday_Case5()
+		{
+			var chocolateBar = new[] { 1, 2, 1, 3, 2, }.ToList();
+			var day = 3;
+			var month = 2;
 
 			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
 			Assert.That(result, Is.EqualTo(2));
