dd68012dc6540e4a64800915e8787f60c95b6fbd
Luca Quaglio <luca.quaglio@hotmail.it>
2022-12-13 19:48:58 +0100

    Subarray division1 (#5)

---
+++ OneMonthPreparationKit/SubarrayDivision1/SubarrayDivision1.cs
@@ -0,0 +1,29 @@
+﻿namespace HackerRank.OneMonthPreparationKit.SubarrayDivision1
+{
+	sealed class SubarrayDivision1
+	{
+		public static int Birthday(List<int> s, int d, int m)
+		{
+			var squares = s;
+			var n = squares.Count;
+
+			int ct = 0;
+			int sum = 0;
+
+			for (int i = 0; i < m - 1; i++)
+			{
+				sum += squares[i];
+			}
+
+			for (int j = m - 1; j < n; j++)
+			{
+				sum -= (j - m) < 0 ? 0 : squares[j - m];
+				sum += squares[j];
+				if (sum == d)
+					ct++;
+			}
+
+			return ct;
+		}
+	}
+}
---
+++ OneMonthPreparationKit/SubarrayDivision1/SubarrayDivision1Test.cs
@@ -0,0 +1,62 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.OneMonthPreparationKit.SubarrayDivision1.Testing
+{
+	sealed class SubarrayDivision1Test
+	{
+		[Test]
+		public void Birthday_Case1()
+		{
+			var chocolateBar = new[] { 2, 2, 1, 3, 2 }.ToList();
+			var day = 4;
+			var month = 2;
+
+			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
+			Assert.That(result, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Birthday_Case2()
+		{
+			var chocolateBar = new[] { 2, 2, 1, 3, 2 }.ToList();
+			var day = 6;
+			var month = 3;
+
+			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
+			Assert.That(result, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Birthday_Case3()
+		{
+			var chocolateBar = new[] { 4 }.ToList();
+			var day = 4;
+			var month = 1;
+
+			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
+			Assert.That(result, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void Birthday_Case4()
+		{
+			var chocolateBar = new[] { 2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1, }.ToList();
+			var day = 18;
+			var month = 7;
+
+			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
+			Assert.That(result, Is.EqualTo(3));
+		}
+
+		[Test]
+		public void Birthday_Case5()
+		{
+			var chocolateBar = new[] { 1, 2, 1, 3, 2, }.ToList();
+			var day = 3;
+			var month = 2;
+
+			var result = SubarrayDivision1.Birthday(chocolateBar, day, month);
+			Assert.That(result, Is.EqualTo(2));
+		}
+	}
+}
