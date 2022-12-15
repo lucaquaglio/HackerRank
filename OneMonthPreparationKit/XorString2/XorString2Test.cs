49b8abcdfcef73bf984f60a5c858eb26457a25e1
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2022-12-15 19:25:55 +0100

    XorString2

---
+++ OneMonthPreparationKit/XorString2/XorString2Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.OneMonthPreparationKit.Testing
+{
+	sealed class XorString2Test
+	{
+		[Test]
+		public void StringXOR_Case1()
+		{
+			var s = "10101";
+			var t = "00101";
+
+			var result = XorString2.StringXOR(s, t);
+
+			Assert.That(result, Is.EqualTo("10000"));
+		}
+
+
+	}
+}
