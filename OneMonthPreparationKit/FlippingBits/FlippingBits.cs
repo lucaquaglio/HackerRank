30e47ce9eaa1d9b50c7785ae972575a8cf809707
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2022-12-16 15:46:15 +0100

    FlippingBits

---
+++ OneMonthPreparationKit/FlippingBits/FlippingBits.cs
@@ -0,0 +1,10 @@
+﻿namespace HackerRank.OneMonthPreparationKit
+{
+	sealed class FlippingBits
+	{
+		public static long Flips(long n)
+		{
+			return ~Convert.ToUInt32(n);
+		}
+	}
+}
---
+++ OneMonthPreparationKit/FlippingBits/FlippingBitsTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.OneMonthPreparationKit.Testing
+{
+	sealed class FlippingBitsTest
+	{
+		[Test]
+		public void FlippingBits_Case1()
+		{
+			var n = 2147483647;
+
+			var result = FlippingBits.Flips(n);
+			Assert.That(result, Is.EqualTo(2147483648));
+		}
+
+		[Test]
+		public void FlippingBits_Case2()
+		{
+			var n = 1;
+
+			var result = FlippingBits.Flips(n);
+			Assert.That(result, Is.EqualTo(4294967294));
+		}
+
+		[Test]
+		public void FlippingBits_Case3()
+		{
+			var n = 0;
+
+			var result = FlippingBits.Flips(n);
+			Assert.That(result, Is.EqualTo(4294967295));
+		}
+
+		[Test]
+		public void FlippingBits_Case4()
+		{
+			var n = 4294967295;
+
+			var result = FlippingBits.Flips(n);
+			Assert.That(result, Is.EqualTo(0));
+		}
+	}
+}
