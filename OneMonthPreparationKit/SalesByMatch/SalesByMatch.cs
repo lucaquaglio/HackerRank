21dff29ed0b8750026ba10179381bdd6cc9c04dc
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2022-12-19 20:56:49 +0100

    SockMerchant

---
+++ OneMonthPreparationKit/SalesByMatch/SalesByMatch.cs
@@ -0,0 +1,19 @@
+﻿namespace HackerRank.OneMonthPreparationKit
+{
+	sealed class SalesByMatch
+	{
+		public static int SockMerchant(int n, List<int> ar)
+		{
+			var typesOfColors = ar.Distinct();
+
+			var result = 0;
+			foreach (var type in typesOfColors)
+			{
+				var pairOfSocks = ar.Count(x => x == type) / 2;
+				result += pairOfSocks;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ OneMonthPreparationKit/SalesByMatch/SalesByMatchTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.OneMonthPreparationKit.Testing
+{
+	sealed class SalesByMatchTest
+	{
+		[Test]
+		public void Scenario1()
+		{
+			var input = new int[] { 1, 2, 1, 2, 1, 3, 2 }.ToList();
+
+			var result = SalesByMatch.SockMerchant(input.Count, input);
+			Assert.That(result, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Scenario2()
+		{
+			var input = new int[] { 10, 20, 20, 10, 10, 30, 50, 10, 20 }.ToList();
+			var result = SalesByMatch.SockMerchant(input.Count, input);
+			Assert.That(result, Is.EqualTo(3));
+		}
+	}
+}
