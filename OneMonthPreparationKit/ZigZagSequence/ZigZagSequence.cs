f2b9625a6286e561b618a185d77f9d1fb9a77604
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2022-12-23 14:49:34 +0100

    Zig Zag Sequence [incomplete]

---
+++ OneMonthPreparationKit/ZigZagSequence/ZigZagSequence.cs
@@ -0,0 +1,32 @@
+﻿namespace HackerRank.OneMonthPreparationKit
+{
+	sealed class ZigZagSequence
+	{
+		public static List<int> FindZigZagSequence(List<int> a, int n)
+		{
+			a.Sort();
+			var mid = (n + 1) / 2;
+			var temp = a[mid];
+			a[mid - 1] = a[n - 1];
+			a[n - 1] = temp;
+
+			var st = mid + 1;
+			var ed = n - 1;
+			while (st < ed)
+			{
+				temp = a[st];
+				a[st] = a[ed];
+				a[ed] = temp;
+				st++;
+				ed--;
+			}
+
+			var result = new List<int>();
+			for (int i = 0; i < n; i++)
+			{
+				result.Add(a[i]);
+			}
+			return result;
+		}
+	}
+}
---
+++ OneMonthPreparationKit/ZigZagSequence/ZigZagSequenceTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.OneMonthPreparationKit.Testing
+{
+	class ZigZagSequenceTest
+	{
+		[Test]
+		public void Scenario1()
+		{
+			var input = new int[] { 2, 3, 5, 1, 4 }.ToList();
+			var result = ZigZagSequence.FindZigZagSequence(input, input.Count);
+
+			Assert.That(result, Is.EqualTo(new int[] { 1, 4, 5, 3, 2 }));
+		}
+
+		[Test]
+		public void Scenario2()
+		{
+			var input = new int[] { 1, 2, 3, 4, 5, 6, 7 }.ToList();
+
+			var result = ZigZagSequence.FindZigZagSequence(input, input.Count);
+
+			Assert.That(result, Is.EqualTo(new int[] { 1, 2, 3, 7, 6, 5, 4, }));
+		}
+	}
+}
