2ae4426b4e8573990173f2cb033a4026eaad9843
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2022-12-23 15:17:27 +0100

    DrawingBook

---
+++ OneMonthPreparationKit/DrawingBook/DrawingBook.cs
@@ -0,0 +1,13 @@
+﻿namespace HackerRank.OneMonthPreparationKit
+{
+	sealed class DrawingBook
+	{
+		public static int PageCount(int n, int p)
+		{
+			var flippedPagesLtR = p / 2;
+			var flippedPagesRtL = (n / 2) - (p / 2);
+
+			return Math.Min(flippedPagesLtR, flippedPagesRtL);
+		}
+	}
+}
---
+++ OneMonthPreparationKit/DrawingBook/DrawingBookTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.OneMonthPreparationKit.Testing
+{
+	sealed class DrawingBookTest
+	{
+		[Test]
+		public void Scenario1()
+		{
+			var n = 5;
+			var p = 3;
+
+			var result = DrawingBook.PageCount(n, p);
+			Assert.That(result, Is.EqualTo(1));
+		}
+
+		[Test]
+		public void Scenario2()
+		{
+			var n = 6;
+			var p = 5;
+
+			var result = DrawingBook.PageCount(n, p);
+			Assert.That(result, Is.EqualTo(1));
+		}
+	}
+}
