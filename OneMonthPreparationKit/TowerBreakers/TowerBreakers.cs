9c9f4b821abd7deefd48181ec657da6d70834571
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2022-12-23 17:25:09 +0100

    TowerBreakers

---
+++ OneMonthPreparationKit/TowerBreakers/TowerBreakers.cs
@@ -0,0 +1,10 @@
+﻿namespace HackerRank.OneMonthPreparationKit
+{
+	sealed class TowerBreakers
+	{
+		public static int Play(int n, int m)
+		{
+			return m != 1 && n % 2 == 1 ? 1 : 2;
+		}
+	}
+}
---
+++ OneMonthPreparationKit/TowerBreakers/TowerBreakersTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.OneMonthPreparationKit
+{
+	sealed class TowerBreakersTest
+	{
+		[Test]
+		public void Scenario1()
+		{
+			var numberOfTowers = 2;
+			var lenghtOfTowers = 6;
+
+			var winner = TowerBreakers.Play(numberOfTowers, lenghtOfTowers);
+
+			Assert.That(winner, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Scenario2()
+		{
+			var numberOfTowers = 2;
+			var lenghtOfTowers = 2;
+
+			var winner = TowerBreakers.Play(numberOfTowers, lenghtOfTowers);
+
+			Assert.That(winner, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void Scenario3()
+		{
+			var numberOfTowers = 1;
+			var lenghtOfTowers = 4;
+
+			var winner = TowerBreakers.Play(numberOfTowers, lenghtOfTowers);
+
+			Assert.That(winner, Is.EqualTo(1));
+		}
+	}
+}
