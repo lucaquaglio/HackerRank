0221bf467ccfa7723c044414da2017a01d92a90f
Luca Quaglio <Luca.Quaglio@wisetechglobal.com>
2023-01-21 18:49:07 +0100

    CaesarCipher

---
+++ OneMonthPreparationKit/CaesarCipher/CaesarCipher.cs
@@ -0,0 +1,30 @@
+﻿namespace HackerRank.OneMonthPreparationKit
+{
+	sealed class CaesarCipher
+	{
+		public static string Encrypt(string s, int k)
+		{
+			var alphabet = new List<char>() { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+			var alphabetUpper = alphabet.Select(x => char.ToUpper(x)).ToList();
+
+			var result = new List<char>();
+			foreach (var c in s)
+			{
+				var actualAlphabet = char.IsUpper(c)
+					? alphabetUpper
+					: alphabet;
+
+				if (!actualAlphabet.Contains(c))
+				{
+					result.Add(c);
+					continue;
+				}
+
+				var index = (actualAlphabet.IndexOf(c) + k) % actualAlphabet.Count;
+				result.Add(actualAlphabet[index]);
+			}
+
+			return new string(result.ToArray());
+		}
+	}
+}
---
+++ OneMonthPreparationKit/CaesarCipher/CaesarCipherTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace HackerRank.OneMonthPreparationKit
+{
+	sealed class CaesarCipherTest
+	{
+		[Test]
+		public void Test1()
+		{
+			var input = "middle-Outz";
+			var result = CaesarCipher.Encrypt(input, 2);
+
+			Assert.That(result, Is.EqualTo("okffng-Qwvb"));
+		}
+
+		[Test]
+		public void Test2()
+		{
+			var input = "Always-Look-on-the-Bright-Side-of-Life";
+			var result = CaesarCipher.Encrypt(input, 5);
+
+			Assert.That(result, Is.EqualTo("Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj"));
+		}
+	}
+}
